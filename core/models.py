f1d08e70f584e5274b9600d3b33b881b2b93b830
mastercoder9363 <118736675+mastercoder9363@users.noreply.github.com>
2022-12-29 18:11:52 +0300

    index

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Cartoon(models.Model):
+    name= models.CharField(max_length=31)
+    photo= models.CharField(max_length=255)
+    about= models.TextField()
+    date= models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.about
--- core/views.py
+++ core/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import *
 
 def index(request):
-    return render(request, 'index.html')
+    cartoons=Cartoon.objects.all()
+    context={
+        'cartoons': cartoons
+    }
+    return render(request, 'index.html', context)
